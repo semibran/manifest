57f447bdf9b57db70ea1ef37586498e865860eeb
Brandon Semilla <semibran@gmail.com>
2017-09-26 10:38:24 -0700

    :boom:

---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = function manifest(node) {
+  if (node !== null && typeof node !== "object") {
+    return document.createTextNode(node)
+  }
+  var tag = node.tag
+  var attributes = node.attributes
+  var children = node.children
+  var element = document.createElement(tag)
+  for (var name in attributes) {
+    var value = attributes[name]
+    element[name] = value
+    element.setAttribute(name, value)
+  }
+  if (children) {
+    for (var i = 0; i < children.length; i++) {
+      var child = children[i]
+      element.appendChild(manifest(child))
+    }
+  }
+  return element
+}
